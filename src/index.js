b50ac30bd788b09f1f6a26a7994ca08dbc8bf888
Linden Daniels <lindendaniels@gmail.com>
2019-10-08 14:37:23 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,13 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from "react-router-dom";
 import './index.css';
-import App from './App';
+import RegistrationForm from './registrationForm/registrationForm';
 
-ReactDOM.render(
-
-<BrowserRouter>
-<App />
-</BrowserRouter>,
-  document.getElementById('root')
-);
+ReactDOM.render(<RegistrationForm />, document.getElementById('root'));
